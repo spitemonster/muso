b416c0617f0f9890d2eb1d58155599eddefd8099
Kirby Johnston <johnstk91@gmail.com>
2024-04-23 23:34:44 -0400

    install aws for file uploads

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -1,7 +1,23 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
 import pg from 'pg'
-import { DB_DATABASE, DB_HOST, DB_USER, DB_PORT } from '$env/static/private'
+import {
+    DB_DATABASE,
+    DB_HOST,
+    DB_USER,
+    DB_PORT,
+    AWS_ACCESS_KEY,
+    AWS_SECRET_KEY,
+} from '$env/static/private'
 import * as schema from '$lib/db/schema/'
+import aws from 'aws-sdk'
+
+aws.config.update({
+    region: 'us-west-2',
+    accessKeyId: AWS_ACCESS_KEY,
+    secretAccessKey: AWS_SECRET_KEY,
+})
+
+export const s3 = new aws.S3()
 
 export const client = new pg.Client({
     host: DB_HOST,
