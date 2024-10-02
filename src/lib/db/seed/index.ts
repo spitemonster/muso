7ca4c2f4e52119511151846b81c3ca0cfb6d20c8
Kirby Johnston <johnstk91@protonmail.com>
2024-10-01 23:25:37 -0400

    rewrite seed to create more realistic fake data

--- src/lib/db/seed/index.ts
+++ src/lib/db/seed/index.ts
@@ -1,84 +1,159 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
 import pg from 'pg'
-// import { DB_DATABASE, DB_HOST, DB_USER, DB_PORT } from '$env/static/private'
 import * as schema from '$lib/db/schema'
 import * as dotenv from 'dotenv'
 
 import { generateUserData } from './users.seed'
 import { generateArtistData } from './artists.seed'
-import { generateAlbumArtistData, generateAlbumData } from './albums.seed'
-import { generateSongArtistData, generateSongData } from './songs.seed'
 import { generateTagData } from './tags.seed'
-import { generateArtistTagData } from './artistTags.seed'
-import { generateAlbumTagData } from './albumTags.seed'
+
+import { generateId } from '$lib/utils'
+
+import type {
+    Album,
+    Song,
+    AlbumArtist,
+    SongArtist,
+    Tag,
+    ArtistTag,
+    AlbumTag,
+} from '$lib/types'
+import { faker } from '@faker-js/faker'
 
 dotenv.config({ path: './.env' })
 
 const main = async () => {
-    const client = new pg.Client({
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        user: process.env.DB_USER,
-        database: process.env.DB_DATABASE,
-    })
+    try {
+        const client = new pg.Client({
+            host: process.env.DB_HOST,
+            port: Number(process.env.DB_PORT),
+            user: process.env.DB_USER,
+            database: process.env.DB_DATABASE,
+        })
 
-    client.connect()
+        client.connect()
 
-    const db = drizzle(client, { schema })
+        const db = drizzle(client, { schema })
 
-    const userCount = 10
-    const artistCount = 250
-    const albumCount = 165
-    const songCount = 5000
-    const tagCount = 100
-    const artistTagCount = 25
-    const albumTagCount = 25
+        const userCount = 10
+        const artistCount = 100
+        const tagCount = 1000
 
-    const userData: (typeof schema.users.$inferInsert)[] =
-        await generateUserData(userCount)
+        const userData: (typeof schema.users.$inferInsert)[] =
+            (await generateUserData(
+                userCount
+            )) as (typeof schema.users.$inferInsert)[]
+        const userIds: string[] = userData.map((u) => u.id)
 
-    const userIds: string[] = userData.map((u) => u.id)
+        const artistData: (typeof schema.artists.$inferInsert)[] =
+            await generateArtistData(artistCount, userIds)
 
-    const artistData: (typeof schema.artists.$inferInsert)[] =
-        await generateArtistData(artistCount, userIds)
-    // const artistIds: string[] = artistData.map((a) => a.id)
+        const tagData: (typeof schema.tags.$inferInsert)[] =
+            await generateTagData(tagCount)
 
-    const albumData: (typeof schema.albums.$inferInsert)[] =
-        await generateAlbumData(albumCount)
-    // const albumIds: string[] = albumData.map((a) => a.id)
+        const artistTagData: (typeof schema.artistTags.$inferInsert)[] = []
+        const albumTagData: (typeof schema.albumTags.$inferInsert)[] = []
 
-    const albumArtistData: (typeof schema.albumArtists.$inferInsert)[] =
-        await generateAlbumArtistData(albumData, artistData)
+        const albumData: (typeof schema.albums.$inferInsert)[] = []
+        const albumArtistData: (typeof schema.albumArtists.$inferInsert)[] = []
+        const songData: (typeof schema.songs.$inferInsert)[] = []
+        const songArtistData: (typeof schema.songArtists.$inferInsert)[] = []
 
-    const songData: (typeof schema.songs.$inferInsert)[] =
-        await generateSongData(songCount, albumData)
-    // const songIds: string[] = songData.map((s) => s.id)
+        for await (const artist of artistData) {
+            const tagCount = Math.ceil(Math.random() * 5)
 
-    const tagData: (typeof schema.tags.$inferInsert)[] =
-        await generateTagData(tagCount)
-    // const tagIds: string[] = tagData.map((t) => t.id)
+            const artistTags: ArtistTag[] = new Array<ArtistTag>(tagCount)
 
-    const artistTagData: (typeof schema.artistTags.$inferInsert)[] =
-        await generateArtistTagData(artistTagCount, artistData, tagData)
-    // const artistTagIds: string[] = artistTagData.map((t) => t.id)
+            for await (let artistTag of artistTags) {
+                const tag: Tag = faker.helpers.arrayElement(tagData) as Tag
 
-    const songArtistData: (typeof schema.songArtists.$inferInsert)[] =
-        await generateSongArtistData(songData, albumArtistData)
+                artistTag = {
+                    id: await generateId(),
+                    artistId: artist.id,
+                    tagId: tag.id,
+                }
 
-    const albumTagData: (typeof schema.albumTags.$inferInsert)[] =
-        await generateAlbumTagData(albumTagCount, albumData, tagData)
+                artistTagData.push(artistTag)
+            }
 
-    await db.insert(schema.users).values(userData)
-    await db.insert(schema.artists).values(artistData)
-    await db.insert(schema.albums).values(albumData)
-    await db.insert(schema.songs).values(songData)
-    await db.insert(schema.tags).values(tagData)
-    await db.insert(schema.artistTags).values(artistTagData)
-    await db.insert(schema.albumArtists).values(albumArtistData)
-    await db.insert(schema.songArtists).values(songArtistData)
-    await db.insert(schema.albumTags).values(albumTagData)
+            const albumCount = Math.ceil(Math.random() * 7)
+            const artistAlbums: Album[] = new Array<Album>(albumCount)
 
-    client.end()
+            for await (let album of artistAlbums) {
+                album = {
+                    id: await generateId(),
+                    title: faker.word.words(Math.ceil(Math.random() * 4)),
+                    coverUrl: `https://picsum.photos/200.webp?${Math.floor(Math.random() * 99)}`,
+                    duration: 0,
+                    songs: [],
+                    artists: [],
+                }
+
+                const albumArtist: AlbumArtist = {
+                    id: await generateId(),
+                    artistId: artist.id,
+                    albumId: album.id,
+                }
+
+                const songCount = Math.floor(Math.random() * 11) + 2
+
+                const albumSongs: Song[] = new Array<Song>(songCount)
+
+                // create songs for album
+                for await (let song of albumSongs) {
+                    song = {
+                        id: await generateId(),
+                        title: faker.word.words(Math.ceil(Math.random() * 4)),
+                        albumId: album.id,
+                        duration: faker.number.int({ min: 11, max: 1200 }),
+                        artists: [],
+                    }
+
+                    const songArtist: SongArtist = {
+                        id: await generateId(),
+                        artistId: artist.id,
+                        songId: song.id,
+                    }
+
+                    album.duration += song.duration
+                    songData.push(song)
+                    songArtistData.push(songArtist)
+                }
+
+                const albumTagCount = Math.ceil(Math.random() * 7)
+                const albumTags = new Array<AlbumTag>(albumTagCount)
+
+                for await (let albumTag of albumTags) {
+                    const tag = faker.helpers.arrayElement(tagData) as Tag
+
+                    albumTag = {
+                        id: await generateId(),
+                        albumId: album.id,
+                        tagId: tag.id,
+                    }
+
+                    albumTagData.push(albumTag)
+                }
+
+                albumData.push(album)
+                albumArtistData.push(albumArtist)
+            }
+        }
+
+        await db.insert(schema.users).values(userData)
+        await db.insert(schema.artists).values(artistData)
+        await db.insert(schema.albums).values(albumData)
+        await db.insert(schema.albumArtists).values(albumArtistData)
+        await db.insert(schema.songs).values(songData)
+        await db.insert(schema.songArtists).values(songArtistData)
+        await db.insert(schema.tags).values(tagData)
+        await db.insert(schema.artistTags).values(artistTagData)
+        await db.insert(schema.albumTags).values(albumTagData)
+
+        client.end()
+    } catch (err) {
+        console.error(err)
+    }
 }
 
 main()
