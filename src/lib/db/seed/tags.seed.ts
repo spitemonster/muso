6db5476716719b7a93bc11f55389d464e12bcf95
Kirby Johnston <johnstk91@protonmail.com>
2024-10-03 16:34:58 -0400

    ensure unique tag names in seed

--- src/lib/db/seed/tags.seed.ts
+++ src/lib/db/seed/tags.seed.ts
@@ -6,12 +6,18 @@ import { generateId } from '$lib/utils'
 
 export async function generateTagData(tagCount: number): Promise<Tag[]> {
     const generatedTagData: Tag[] = []
+    const assignedTagNames = new Set<string>()
 
     for (let i = 0; i < tagCount; i++) {
+        let name: string
+
+        do {
+            name = faker.word.words(Math.max(Math.round(Math.random() * 2), 1))
+        } while (assignedTagNames.has(name))
+
+        assignedTagNames.add(name)
+
         const id = await generateId()
-        const name = faker.word.words(
-            Math.max(Math.round(Math.random() * 2), 1)
-        )
         const slug = name.replaceAll(' ', '-')
 
         generatedTagData.push({
