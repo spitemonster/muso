cf3bf0882ec4631b88ed4b1d48ef45bf466e9b03
Kirby Johnston <johnstk91@protonmail.com>
2024-10-03 16:31:32 -0400

    start rewrite of artist controller/controller layouts

--- src/lib/db/controllers/artist.controller.ts
+++ src/lib/db/controllers/artist.controller.ts
@@ -1,57 +1,97 @@
-import type { Artist, Tag } from '$lib/types'
-import {
-    getArtistFromDbById,
-    getArtistsFromDbByUserId,
-    getArtistsFromDbByUserEmail,
-    getUserFromDbById,
-    getArtistsByTagId,
-    getArtistTags,
-    getRandomArtists,
-} from '$lib/db/utils'
-import { createArtistDbRecord } from '../utils/artist.utils'
+import type { Artist, Tag, ArtistTag, User } from '$lib/types'
+import { eq, sql } from 'drizzle-orm'
+// import {
+
+// } from '$lib/db/utils'
+// import { createArtistDbRecord } from '../utils/artist.utils'
+import { db } from '../db'
+import * as schema from '../schema'
 
 export class ArtistController {
-    static async CreateArtist(
-        adminId: string,
-        id: string,
-        name: string,
-        url: string = ''
-    ): Promise<Artist | null> {
-        const admin = getUserFromDbById(adminId)
+    // static async CreateArtist(
+    //     adminId: string,
+    //     id: string,
+    //     name: string,
+    //     url: string = ''
+    // ): Promise<Artist> {
+    //     const admin = getUserFromDbById(adminId)
 
-        if (!admin) {
-            return null
-        }
+    //     if (!admin) {
+    //         return null
+    //     }
 
-        return await createArtistDbRecord(adminId, id, name, url)
+    //     return await createArtistDbRecord(adminId, id, name, url)
+    // }
+
+    static async FindArtistById(id: string): Promise<Artist> {
+        const result = await db.query.artists.findFirst({
+            where: eq(schema.artists.id, id),
+        })
+
+        if (!result) return null
+
+        return result as Artist
     }
 
-    static async FindArtistById(
-        id: string,
-        include: string[]
-    ): Promise<Artist | null> {
-        return await getArtistFromDbById(id, include)
+    static async FindArtistBySlug(slug: string): Promise<Artist> {
+        const result = await db.query.artists.findFirst({
+            where: eq(schema.artists.slug, slug),
+        })
+
+        if (!result) return null
+
+        return result as Artist
     }
 
-    static async FindArtistsByUserId(userId: string): Promise<Artist[] | null> {
-        return await getArtistsFromDbByUserId(userId)
+    static async FindArtistsAdminedByUser(user: User): Promise<Artist[]> {
+        // TODO: setup ArtistAdmin schema
+        return []
     }
 
-    static async FindArtistsByUserEmail(
-        userEmail: string
-    ): Promise<Artist[] | null> {
+    // static async FindArtistsAdminedByUserId(userId: string): Promise<Artist[]> {
+    //     return await getArtistsFromDbByUserId(userId)
+    // }
+
+    static async FindArtistsByUserEmail(userEmail: string): Promise<Artist[]> {
         return await getArtistsFromDbByUserEmail(userEmail)
     }
 
-    static async FindArtistsByTagId(tagId: string): Promise<Artist[] | null> {
-        return await getArtistsByTagId(tagId)
+    static async FindArtistsWithTagById(tagId: string): Promise<Artist[]> {
+        const artistTags = await db.query.artistTags.findMany({
+            where: eq(schema.tags.id, tagId),
+            with: {
+                artist: true,
+            },
+        })
+
+        if (!artistTags) return []
+
+        return artistTags.map((at: ArtistTag) => at!.artist)
     }
 
-    static async GetArtistTags(artistId: string): Promise<Tag[] | null> {
-        return await getArtistTags(artistId)
+    static async FindArtistsWithTag(tag: User): Promise<Artist[]> {
+        return await this.FindArtistsWithTagById(tag!.id)
     }
 
     static async GetRandomArtists(count: number): Promise<Artist[]> {
-        return await getRandomArtists(count)
+        const randArtists = await db.query.artists.findMany({
+            columns: {
+                name: true,
+                id: true,
+                slug: true,
+                createdAt: true,
+                adminId: true,
+                url: true,
+                location: true,
+                biography: true,
+                profileImageUrl: true,
+            },
+            orderBy: sql`RANDOM()`,
+            limit: count,
+        })
+
+        if (!randArtists) return []
+
+        return randArtists as Artist[]
     }
 }
