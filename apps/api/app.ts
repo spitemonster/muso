825179d67b3d868b2dc3956c2506e7c70dc296c1
Kirby Johnston <sup@kirbyjohnston.com>
2026-08-20 23:50:40 -0400

    feat: basic api server and graphql setup

---
+++ apps/api/app.ts
@@ -0,0 +1,48 @@
+import { Hono } from 'hono'
+import { prettyJSON } from 'hono/pretty-json'
+import { serve } from '@hono/node-server'
+import { createYoga } from 'graphql-yoga'
+import { buildSchema } from 'drizzle-graphql'
+import { db } from '@muso/db/db'
+
+const { schema } = buildSchema(db, {
+	mutations: false
+});
+
+const yoga = createYoga({
+	schema,
+	graphqlEndpoint: '/graphql'
+})
+
+const app = new Hono()
+app.get('/', (ctx) => ctx.text('API'))
+
+app.all('/graphql', async (c) => {
+	const response = await yoga.handle(c.req.raw, {
+		req: c.req,
+		db,
+	})
+	return response
+})
+
+
+app.use(prettyJSON())
+
+app.notFound((c) => c.json({ message: 'Not Found', ok: false }, 404))
+
+const server = serve({ fetch: app.fetch, port: 8888 })
+
+// graceful shutdown
+process.on('SIGINT', () => {
+	server.close()
+	process.exit(0)
+})
+process.on('SIGTERM', () => {
+	server.close((err) => {
+		if (err) {
+			console.error(err)
+			process.exit(1)
+		}
+		process.exit(0)
+	})
+})
