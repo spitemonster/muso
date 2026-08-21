7f00d39938bfef7c7841f6f5a8b2ccab4125fba9
Kirby Johnston <sup@kirbyjohnston.com>
2026-08-21 17:05:42 -0400

    feat: default sveltekit setup importing button from ui library

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Button } from '@muso/ui';
+</script>
+
+<h1 class="text-2xl font-bold text-brand-900">Welcome to muso</h1>
+<p class="mt-2 text-sm">
+	Visit <a class="underline" href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+	documentation
+</p>
+
+<Button style="solid">Hello from packages/ui</Button>
