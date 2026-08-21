412a0e386e1719d60ae80420686310232a1472d4
Kirby Johnston <sup@kirbyjohnston.com>
2026-08-21 18:11:58 -0400

    feat: initial admin sveltekit app setup

---
+++ apps/admin/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { Button } from '@muso/ui';
+</script>
+
+<h1 class="text-2xl font-bold text-brand-900">Muso admin</h1>
+
+<Button style="solid">Hello from packages/ui</Button>
