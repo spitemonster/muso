f7218cb1cb8e502ff9eba2049cb8a7d0e82234b4
Kirby Johnston <sup@kirbyjohnston.com>
2026-08-21 21:32:10 -0400

    chore: general commits

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
-	import { Button } from '@muso/ui';
+	import { Button } from '@muso/ui'
+	import PageTemplate from '$lib/globals/PageTemplate.svelte'
 </script>
 
-<h1 class="text-2xl font-bold text-brand-900">Welcome to muso</h1>
-<p class="mt-2 text-sm">
-	Visit <a class="underline" href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-	documentation
-</p>
-
-<Button style="solid">Hello from packages/ui</Button>
+<h1>muso</h1>
+<p>You made the right choice.</p>
