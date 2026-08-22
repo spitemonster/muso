b946f605913f21e62238aa116565a316c4b52b8f
Kirby Johnston <sup@kirbyjohnston.com>
2026-08-21 20:23:42 -0400

    feat: base heading component and story setup

---
+++ packages/ui/src/lib/components/Heading/Heading.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte'
+
+
+import type { HTMLAttributes } from 'svelte/elements';
+import { tv } from 'tailwind-variants';
+
+type Props = HTMLAttributes<HTMLHeadingElement> & {
+    children?: Snippet
+    level: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+	size?: 'base' | 'lg' | 'xl' | '2xl' | '3xl' | 'display-sm' | 'display-md' | 'display-lg'
+}
+
+const { children, level, size, ...rest } = $props();
+
+const headingClass = tv({
+	base: '',
+	variants: {
+		size: {
+			base: 'text-base',
+			lg: 'text-lg',
+			xl: 'text-xl',
+			'2xl': 'text-2xl',
+			'3xl': 'text-3xl',
+			'display-sm': 'text-display-sm',
+			'display-md': 'text-display-md',
+			'display-lg': 'text-display-lg',
+		},
+	},
+	defaultVariants: { size: 'xl' },
+})
+
+</script>
+
+<svelte:element this={level} class={headingClass({ size })} {...rest}>
+    {@render children?.()}
+</svelte:element>
