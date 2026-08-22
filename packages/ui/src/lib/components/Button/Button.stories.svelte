d60aa002a34745f09e51cee285c0ff18c81e750e
Kirby Johnston <sup@kirbyjohnston.com>
2026-08-21 20:23:30 -0400

    feat: initial base button story

---
+++ packages/ui/src/lib/components/Button/Button.stories.svelte
@@ -0,0 +1,21 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf'
+	import Button from './Button.svelte'
+
+	const { Story } = defineMeta({
+		title: 'UI/Components/Button',
+		component: Button,
+		argTypes: {
+			style: {
+				control: {
+					type: 'select',
+					options: ['solid', 'outline'],
+					description: 'Change button style.',
+				},
+			},
+		},
+	})
+</script>
+
+<Story name="Solid" args={{ style: 'solid' }}>Solid Button</Story>
+<Story name="Outline" args={{ style: 'outline' }}>Outline Button</Story>
