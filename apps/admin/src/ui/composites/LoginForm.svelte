b4cf90916384121452f7e6e4fabcd8a5252f7edf
Kirby Johnston <sup@kirbyjohnston.com>
2026-08-23 00:01:08 -0400

    feat: add base login form component

---
+++ apps/admin/src/ui/composites/LoginForm.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import Container from '../global/Container.svelte'
+	import { TextInput } from '@muso/ui'
+</script>
+
+<form action="" class="grid grid-cols-2 gap-4 max-w-content-max-width">
+	<TextInput
+		label="First Name"
+		name="first-name"
+		type="text"
+		autocomplete="given-name"
+		outerClass="col-span-2 sm:col-span-1"
+		placeholder="David"
+		required
+	/>
+	<TextInput
+		label="Last Name"
+		name="last-name"
+		type="text"
+		autocomplete="family-name"
+		outerClass="col-span-2 sm:col-span-1"
+		placeholder="Duchovny"
+		required
+	/>
+	<TextInput
+		label="Email"
+		name="email"
+		type="email"
+		autocomplete="email"
+		placeholder="david@duchovny.com"
+		outerClass="col-span-2 sm:col-span-1"
+		required
+	/>
+	<TextInput
+		label="Phone"
+		name="phone"
+		type="tel"
+		autocomplete="tel"
+		placeholder="1234567890"
+		outerClass="col-span-2 sm:col-span-1"
+		required
+	/>
+</form>
