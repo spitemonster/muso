64b2a6836b7f08af4d91ef942dd02ef91dff6359
Kirby Johnston <sup@kirbyjohnston.com>
2026-08-22 23:20:46 -0400

    feat: add base general purpose container component

---
+++ apps/admin/src/ui/global/Container.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { tv } from 'tailwind-variants'
+	interface Props {
+		wide?: boolean
+	}
+
+	const calcClass = tv({
+		base: 'mx-auto px-4',
+		variants: {
+			wide: {
+				false: 'max-w-content-max-width',
+				true: 'max-w-content-wide-width',
+			},
+		},
+	})
+
+	let { children, wide = false, class: className = '', ...attrs } = $props()
+</script>
+
+<div class={calcClass({ wide, class: className })} {...attrs}>
+	{@render children?.()}
+</div>
