b5fa9c4c554489f75d72a405cd89d5e6132e3342
Kirby Johnston <sup@kirbyjohnston.com>
2026-08-23 00:00:44 -0400

    feat: tailwind variants and props for container input

--- apps/admin/src/ui/global/Container.svelte
+++ apps/admin/src/ui/global/Container.svelte
@@ -1,22 +1,37 @@
 <script lang="ts">
+	import type { Snippet } from 'svelte'
 	import { tv } from 'tailwind-variants'
 	interface Props {
+		children?: Snippet
+		align: 'start' | 'center' | 'end'
+		class?: string
 		wide?: boolean
 	}
 
 	const calcClass = tv({
-		base: 'mx-auto px-4',
+		base: 'px-4',
 		variants: {
 			wide: {
 				false: 'max-w-content-max-width',
 				true: 'max-w-content-wide-width',
 			},
+			align: {
+				start: 'mr-auto',
+				center: 'mx-auto',
+				end: 'ml-auto',
+			},
 		},
 	})
 
-	let { children, wide = false, class: className = '', ...attrs } = $props()
+	let {
+		children,
+		wide = false,
+		align = 'center',
+		class: className = '',
+		...attrs
+	}: Props = $props()
 </script>
 
-<div class={calcClass({ wide, class: className })} {...attrs}>
+<div class={calcClass({ wide, align, class: className })} {...attrs}>
 	{@render children?.()}
 </div>
