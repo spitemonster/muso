5428e51664fd757397778634194ee9527221dcd2
Kirby Johnston <sup@kirbyjohnston.com>
2026-08-22 23:59:55 -0400

    feat: add base textinput component

--- packages/ui/src/lib/index.ts
+++ packages/ui/src/lib/index.ts
@@ -1,2 +1,3 @@
 export { default as Button } from './components/Button/Button.svelte'
 export { default as Heading } from './components/Heading/Heading.svelte'
+export { default as TextInput } from './components/form/TextInput/TextInput.svelte'
