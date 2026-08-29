943883a7001bafb912c966552e8fc434ad7a8f6a
Kirby Johnston <sup@kirbyjohnston.com>
2026-08-29 13:33:24 -0400

    feat: add password input

--- packages/ui/src/lib/index.ts
+++ packages/ui/src/lib/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './components/Button/Button.svelte'
 export { default as Heading } from './components/Heading/Heading.svelte'
 export { default as TextInput } from './components/form/TextInput/TextInput.svelte'
+export { default as PasswordInput } from './components/form/PasswordInput/PasswordInput.svelte'
