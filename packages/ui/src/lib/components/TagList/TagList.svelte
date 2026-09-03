8204421ef72f7df27ee9c1481ca9844e11eda625
Kirby Johnston <sup@kirbyjohnston.com>
2026-09-03 14:33:25 -0400

    refactor: rename tag card list to tag list and tag card to tag list item, update stories

---
+++ packages/ui/src/lib/components/TagList/TagList.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { Tag } from '../../types'
+	import { TagListItem } from '@muso/ui'
+
+	interface Props {
+		tags: Tag[]
+		size?: 'sm' | 'md' | 'lg'
+		icon?: boolean
+		class?: string
+		[key: string]: unknown
+	}
+
+	const { tags, size, icon = true, class: className = '', ...attrs }: Props = $props()
+</script>
+
+<ul class={`flex flex-wrap gap-3 justify-start ${className}`} {...attrs}>
+	{#each tags as tag}
+		<TagListItem {tag} {size} {icon} />
+	{/each}
+</ul>
---
+++ packages/ui/src/lib/components/TagList/fakeTags.ts
@@ -0,0 +1,129 @@
+import type { Tag } from '../../types';
+
+export const fakeTags: Tag[] = [
+	{
+		"id": "0e4d331c-b494-4dd7-98d7-3b174b562555",
+		"name": "excellent",
+		"slug": "excellent"
+	},
+	{
+		"id": "be4500f0-4ecd-4954-b07e-bcde813dec96",
+		"name": "excitedly amid",
+		"slug": "excitedly-amid"
+	},
+	{
+		"id": "b68fd3fa-4c8d-4f71-bd19-9cfad47464bb",
+		"name": "because",
+		"slug": "because"
+	},
+	{
+		"id": "102f87fb-b4fb-423c-a72f-7ba1b334b7d9",
+		"name": "large",
+		"slug": "large"
+	},
+	{
+		"id": "14e42190-9a98-4244-8cdd-a75d8b2f4dae",
+		"name": "spattering",
+		"slug": "spattering"
+	},
+	{
+		"id": "4016bc4f-711f-4097-abd7-908088e48a2e",
+		"name": "daintily",
+		"slug": "daintily"
+	},
+	{
+		"id": "01d68182-a671-406a-83a4-fa15399b28c5",
+		"name": "plus",
+		"slug": "plus"
+	},
+	{
+		"id": "54a92b2d-4086-4c11-8565-0eda6a760bca",
+		"name": "below",
+		"slug": "below"
+	},
+	{
+		"id": "921adfa5-3e95-40b4-8060-dd9f8927e86a",
+		"name": "enlist wildly",
+		"slug": "enlist-wildly"
+	},
+	{
+		"id": "0c16e097-e9db-47ca-831d-2528de317430",
+		"name": "jot firm",
+		"slug": "jot-firm"
+	},
+	{
+		"id": "9fcd1814-d970-48d9-ade4-2fb41d239c56",
+		"name": "after",
+		"slug": "after"
+	},
+	{
+		"id": "19d83a30-3f47-42a4-8cb2-23d825062fe6",
+		"name": "clone",
+		"slug": "clone"
+	},
+	{
+		"id": "bfd4d5a3-2778-41f3-8543-a99e6bb7081d",
+		"name": "direct",
+		"slug": "direct"
+	},
+	{
+		"id": "b0c7a46b-b5ea-403a-81a9-76c5bc1dc798",
+		"name": "whole",
+		"slug": "whole"
+	},
+	{
+		"id": "0b8cce6f-e5ea-493c-9109-d61ccf58e87a",
+		"name": "stratify incline",
+		"slug": "stratify-incline"
+	},
+	{
+		"id": "4a03c677-0e80-4eac-ae49-b12d31e39043",
+		"name": "tabulate",
+		"slug": "tabulate"
+	},
+	{
+		"id": "4d29f835-6ea7-4e15-8327-aa9338fdb4ee",
+		"name": "alliance",
+		"slug": "alliance"
+	},
+	{
+		"id": "1f05f06e-d80b-42b4-b8b0-5d58dca29113",
+		"name": "brr",
+		"slug": "brr"
+	},
+	{
+		"id": "25353279-9c4e-4b00-b089-d23a785715e5",
+		"name": "amid",
+		"slug": "amid"
+	},
+	{
+		"id": "ca3d7017-51b1-4782-943f-9a09d1c34dbf",
+		"name": "whenever",
+		"slug": "whenever"
+	},
+	{
+		"id": "327e4582-5e7b-47ec-b288-46bc11fb1ca7",
+		"name": "appreciate",
+		"slug": "appreciate"
+	},
+	{
+		"id": "9956fcce-b1b8-40b7-adfe-79de7a56a9e0",
+		"name": "abaft",
+		"slug": "abaft"
+	},
+	{
+		"id": "a4ec9f75-5c8d-4ce9-87da-66da287642cc",
+		"name": "fatherly",
+		"slug": "fatherly"
+	},
+	{
+		"id": "a9074496-aab0-4df3-8501-bdc88fe7834d",
+		"name": "gradient",
+		"slug": "gradient"
+	},
+	{
+		"id": "4f05a22b-f85f-4d61-8008-e90d9aabfc2c",
+		"name": "helplessly queasily",
+		"slug": "helplessly-queasily"
+	}
+];
