9812f0608c1ccba9cd2116635a2947fc55f73bba
Kirby Johnston <sup@kirbyjohnston.com>
2026-09-03 20:37:55 -0400

    chore: clean up drizzle imports

--- packages/db/schema/enums.ts
+++ packages/db/schema/enums.ts
@@ -1,4 +1,5 @@
 import { pgEnum } from 'drizzle-orm/pg-core';
 
-export const statusEnum = pgEnum('status', ['draft', 'published', 'private', 'trashed']);
+export const releaseStatusEnum = pgEnum('status', ['draft', 'published', 'private', 'trashed']);
 export const collectionTypeEnum = pgEnum('collection_type', ['album', 'ep', 'single', 'compilation']);
+export const userTypeEnum = pgEnum('user_type', ['user', 'admin']);
--- packages/db/schema/trackTags.schema.ts
+++ packages/db/schema/trackTags.schema.ts
@@ -1,5 +1,5 @@
 import { relations } from 'drizzle-orm';
-import { text, timestamp, pgTable, primaryKey, uuid } from 'drizzle-orm/pg-core';
+import { pgTable, primaryKey, uuid } from 'drizzle-orm/pg-core';
 
 import * as schema from '.';
 
